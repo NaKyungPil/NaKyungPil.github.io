0cce203ce4ccf67a15755a3f0c6b2cad0eca4530
NaKyungPil <127269932+NaKyungPil@users.noreply.github.com>
2023-05-24 12:03:57 +0900

    Add files via upload

---
+++ Finacial Statements/035420.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import requests
+code = "035420"
+URL = f"https://finance.naver.com/item/main.naver?code={code}"
+r = requests.get(URL)
+df = pd.read_html(r.text)[3]
+df = df.replace({np.nan: ""})
+
+html = df.to_html(justify='center', index = False)
+print(html)
