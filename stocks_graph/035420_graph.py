4836439d798c4d86dc4af5bd358ca7b623d0a185
NaKyungPil <127269932+NaKyungPil@users.noreply.github.com>
2023-06-03 03:34:37 +0900

    Add files via upload

---
+++ stocks_graph/035420_graph.py
@@ -0,0 +1,17 @@
+import FinanceDataReader as fd
+import plotly.graph_objects as go
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+start_date = datetime.today()-relativedelta(years=2)
+end_date = datetime.today()
+df = fd.DataReader('035420',start_date,end_date)
+
+
+# https://plotly.com/python/statistical-charts/ 참조
+
+fig = go.Figure(data=go.Scatter(x=df.index,y=df['Close']))
+fig.update_layout(title='NAVER',xaxis_title='날짜',yaxis_title='주가')
+
+html = 'C:/Users/ktf98/OneDrive/바탕 화면/웹프/G.I/NAVER.html'
+fig.write_html(html)
